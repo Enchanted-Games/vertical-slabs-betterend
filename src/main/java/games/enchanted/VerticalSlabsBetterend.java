cf5dd3613c205b7450272fac852d14032dd93cc6
Enchanted Games <isaacoblackshaw@gmail.com>
2022-04-01 15:08:33 +0100

    Seperate betterend code from main mod
    TODO: recipes and models

---
+++ src/main/java/games/enchanted/VerticalSlabsBetterend.java
@@ -0,0 +1,41 @@
+package games.enchanted;
+
+import games.enchanted.registry.ModFlammableBlocks;
+import games.enchanted.addons.betterEnd.BetterendAddon;
+import net.fabricmc.api.ModInitializer;
+import net.minecraft.MinecraftVersion;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class VerticalSlabsBetterend implements ModInitializer{
+
+    // TODO
+    // make neon catus have np shading on model
+    // same with smaragdant blocks
+
+    // mod id
+    public static final String MOD_ID = "evs-betterend";
+    public static final String loggerID = "[" + MOD_ID + "] ";
+    public static String minecraftCurrentVersion = MinecraftVersion.CURRENT.getReleaseTarget();
+    // logger
+    public static final Logger LOGGER = LogManager.getLogger("Enchanted Vertical Slabs - Betterend addon");
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info(loggerID + "Detected Minecraft " + minecraftCurrentVersion + " (release target)");
+        LOGGER.info(loggerID + "Starting to load mod...");
+
+        // looads the better end addon class
+        BetterendAddon.build();
+
+        // registers flammable blocks
+        ModFlammableBlocks.registerFlammables();
+
+        // logs to console that the mod is ready
+        LOGGER.info(loggerID + "Enchanted Vertical Slabs Betterend addon initialized!");
+    }
+
+
+    
+    
+}
---
+++ src/main/java/games/enchanted/registry/ModFlammableBlocks.java
@@ -0,0 +1,25 @@
+package games.enchanted.registry;
+
+import games.enchanted.addons.betterEnd.BetterendBlocks;
+import net.fabricmc.fabric.api.registry.FlammableBlockRegistry;
+
+public class ModFlammableBlocks {
+
+    public static void registerFlammables(){
+        // Flammable blocks
+        int Encouragement = 5;
+        int Flammability = 20;
+
+        FlammableBlockRegistry.getDefaultInstance().add(BetterendBlocks.VERTICAL_DRAGON_TREE_SLAB, Encouragement, Flammability);
+        FlammableBlockRegistry.getDefaultInstance().add(BetterendBlocks.VERTICAL_LUCERNIA_SLAB, Encouragement, Flammability);
+        FlammableBlockRegistry.getDefaultInstance().add(BetterendBlocks.VERTICAL_MOSSY_GLOWSHROOM_SLAB, Encouragement, Flammability);
+        FlammableBlockRegistry.getDefaultInstance().add(BetterendBlocks.VERTICAL_PYTHADENDRON_SLAB, Encouragement, Flammability);
+        FlammableBlockRegistry.getDefaultInstance().add(BetterendBlocks.VERTICAL_TENANEA_SLAB, Encouragement, Flammability);
+        FlammableBlockRegistry.getDefaultInstance().add(BetterendBlocks.VERTICAL_END_LOTUS_SLAB, Encouragement, Flammability);
+        FlammableBlockRegistry.getDefaultInstance().add(BetterendBlocks.VERTICAL_HELIX_TREE_SLAB, Encouragement, Flammability);
+        FlammableBlockRegistry.getDefaultInstance().add(BetterendBlocks.VERTICAL_JELLYSHROOM_SLAB, Encouragement, Flammability);
+        FlammableBlockRegistry.getDefaultInstance().add(BetterendBlocks.VERTICAL_LACUSGROVE_SLAB, Encouragement, Flammability);
+        FlammableBlockRegistry.getDefaultInstance().add(BetterendBlocks.VERTICAL_UMBRELLA_TREE_SLAB, Encouragement, Flammability);
+    }
+
+}
